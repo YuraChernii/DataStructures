7bf7e2ee5dba739c18640e62e01b836a0407d002
Yurii Chernii <yurii.chernii.alpha@gmail.com>
2024-08-28 15:35:56 +0300

    add FenwickTree

---
+++ DataStructures.UnitTests/FenwickTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using DataStructures.DataStructures;
+
+namespace DataStructures.UnitTests
+{
+    public class FenwickTreeTests
+    {
+        [Fact]
+        public void Should_Update()
+        {
+            List<int> initialData = [ 1, 1, 1, 1, 1, 1 ];
+            FenwickTree fenwickTree = new(initialData);
+
+            fenwickTree.Update(6, 2);
+
+            Assert.Equal(3, fenwickTree.Query(3)); // (1 + 1 + 1 + .. + .. + ..)
+            Assert.Equal(4, fenwickTree.RangeQuery(2, 5)); // (.. + 1 + 1 + 1 + 1 + ..)
+            Assert.Equal(8, fenwickTree.Query(6)); // (1 + 1 + 1 + 1 + 1 + 3)
+        }
+    }
+}
---
+++ DataStructures/DataStructures/FenwickTree.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.DataStructures
+{
+    public class FenwickTree
+    {
+        private readonly int[] tree;
+
+        public FenwickTree(IList<int> initialData)
+        {
+            tree = new int[initialData.Count + 1];
+
+            for (int i = 0; i < initialData.Count; i++)
+            {
+                Update(i + 1, initialData[i]);
+            }
+        }
+
+        public void Update(int index, int value)
+        {
+            while (index < tree.Length)
+            {
+                tree[index] += value;
+                index += index & -index;
+            }
+        }
+
+        public int Query(int index)
+        {
+            int sum = 0;
+
+            while (index > 0)
+            {
+                sum += tree[index];
+                index -= index & -index;
+            }
+
+            return sum;
+        }
+
+        public int RangeQuery(int left, int right) =>
+            Query(right) - Query(left - 1);
+    }
+}
